141a0a6df017c6cd1e6efb5e287bb7f8e4aab2e2
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2020-05-16 01:48:19 -0500

    Improving code

--- products/views.py
+++ products/views.py
@@ -5,33 +5,19 @@ from .models import Product
 
 class GetProducts(views.View):
     def get(self, request):
-        # if request.GET.get('search') and request.GET.get('category'):
-        #     print("llego")
-        #     value = request.GET['search']
-        #     category = request.GET['category']
-        #     products = Product.objects.filter(
-        #         Q(name__icontains=value),
-        #         category=category,
-        #         is_active=True
-        #     )
-        # else:
-        #     print("NO llego")
-        #     products = Product.objects.filter(is_active=True)
-        # products = Product.objects.all()
-        # print("P1", products)
-        # products = products.filter(is_active=True)
-        # print("P2", products)
         products = Product.objects.filter(is_active=True)
-        if request.GET.get('search'):
-            search = request.GET['search']
+        search = request.GET['search'] if request.GET.get('search') else None
+        category = request.GET['category'] if request.GET.get('category') else None
+        if search:
             products = products.filter(Q(name__icontains=search))
-        if request.GET.get('category'):
-            category = request.GET['category']
+        if category:
             products = products.filter(category=category)
         counter = len(products)
         template_name = 'products/list.html'
         context = {
             'products': products,
-            'counter': counter
+            'counter': counter,
+            'search': search,
+            'category': category
         }
         return render(request, template_name, context)
